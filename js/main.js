9b56fb0076957ed8680f1bcb86bad2978cc07f08
echocbx <liusx0619@qq.com>
2023-06-27 20:53:35 +0800

    Site updated: 2023-06-27 20:53:31

--- js/main.js
+++ js/main.js
@@ -281,9 +281,8 @@ document.addEventListener('DOMContentLoaded', function () {
         const currentTop = window.scrollY || document.documentElement.scrollTop
         const isDown = scrollDirection(currentTop)
         if (currentTop > 56) {
-          $header.classList.add('is-top-bar','nav-visible')
           if (isDown) {
-            if ($header.classList.contains('nav-visible')) $header.classList.add('nav-visible')
+            if ($header.classList.contains('nav-visible')) $header.classList.remove('nav-visible')
             if (isChatBtnShow && isChatShow === true) {
               chatBtnHide()
               isChatShow = false
