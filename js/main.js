3d941b5856a7e1c4e3c3b7ac9ab322a5a44d7eed
echocbx <liusx0619@qq.com>
2023-07-29 19:39:32 +0800

    Site updated: 2023-07-29 19:39:27

--- js/main.js
+++ js/main.js
@@ -94,18 +94,24 @@ document.addEventListener('DOMContentLoaded', function () {
       if (document.queryCommandSupported && document.queryCommandSupported('copy')) {
         document.execCommand('copy')
         if (GLOBAL_CONFIG.Snackbar !== undefined) {
-          btf.snackbarShow(GLOBAL_CONFIG.copy.success)
+          new Vue({
+            data: function () {
+                this.$notify({
+                    title: "复制成功",
+                    message: "转载请遵守cc协议",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 4000
+                });
+            }
+          })
         } else {
           const prevEle = ctx.previousElementSibling
           prevEle.innerText = GLOBAL_CONFIG.copy.success
           prevEle.style.opacity = 1
           setTimeout(() => { prevEle.style.opacity = 0 }, 700)
-        }
-      } else {
-        if (GLOBAL_CONFIG.Snackbar !== undefined) {
-          btf.snackbarShow(GLOBAL_CONFIG.copy.noSupport)
-        } else {
-          ctx.previousElementSibling.innerText = GLOBAL_CONFIG.copy.noSupport
         }
       }
     }
@@ -281,8 +287,9 @@ document.addEventListener('DOMContentLoaded', function () {
         const currentTop = window.scrollY || document.documentElement.scrollTop
         const isDown = scrollDirection(currentTop)
         if (currentTop > 56) {
+          $header.classList.add('is-top-bar','nav-visible')
           if (isDown) {
-            if ($header.classList.contains('nav-visible')) $header.classList.remove('nav-visible')
+            if ($header.classList.contains('nav-visible')) $header.classList.add('nav-visible')
             if (isChatBtnShow && isChatShow === true) {
               chatBtnHide()
               isChatShow = false
